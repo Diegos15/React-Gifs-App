7272418b443c2a10c1f847ba7b221cd93f9ea9a5
Diegos15 <diegos2770@gmail.com>
2020-10-12 18:40:41 -0500

    Cambio de build a docs

---
+++ docs/precache-manifest.8976f4775162ef4060c0f5a0338c74aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d39b54344a5df806241ea0ba6b5c369",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ccf9f72479eed1f031d8",
+    "url": "/static/css/main.264da754.chunk.css"
+  },
+  {
+    "revision": "cbf2febe82ca3f3bd393",
+    "url": "/static/js/2.582dbd2e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.582dbd2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccf9f72479eed1f031d8",
+    "url": "/static/js/main.c62fc1d6.chunk.js"
+  },
+  {
+    "revision": "f4ad07f6c80c5cea4d39",
+    "url": "/static/js/runtime-main.9cdc9bbb.js"
+  }
+]);
